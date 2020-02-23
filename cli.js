299b7e5e1765f33287a545bff97df5292448d2bc
NumberCooler <fsh.kod@gmail.com>
2020-02-23 10:25:12 -0300

    init

---
+++ cli.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+var dir = process.cwd();
+app.use(express.static(dir));
+app.listen(8040,()=> {
+    console.log("listening "+8040 + " with " + dir);
+});
